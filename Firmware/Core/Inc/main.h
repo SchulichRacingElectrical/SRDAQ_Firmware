f66b4632123eedb99ec9e06fd01aebea9e32133b
Will Ehman <will@ehman.com>
2020-02-05 15:10:52 -0700

    enabling all 16 adc inputs and can and reducing sampling rate to 480 cycles

--- Firmware/Core/Inc/main.h
+++ Firmware/Core/Inc/main.h
@@ -64,6 +64,8 @@ void Error_Handler(void);
 #define A0_GPIO_Port GPIOA
 #define LD3_Pin GPIO_PIN_14
 #define LD3_GPIO_Port GPIOB
+#define SPI_CS_Pin GPIO_PIN_2
+#define SPI_CS_GPIO_Port GPIOG
 #define LD2_Pin GPIO_PIN_7
 #define LD2_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
